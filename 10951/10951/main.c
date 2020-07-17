e6d40f8e6d4c10ff9f446232c17c9e96e02401f6
SeongBinYoon <55236612+SeongBinYoon@users.noreply.github.com>
2020-07-17 14:12:20 +0900

    Add files via upload

---
+++ 10951/10951/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  10951
+//
+//  Created by SeongBinYoon on 2020/03/09.
+//  Copyright © 2020 SeongBinYoon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    while(scanf("%d %d", &a, &b) != EOF){
+        printf("%d\n", a + b);
+    }
+}
