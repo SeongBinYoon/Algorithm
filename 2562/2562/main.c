a89649c22eafbc7bb5479835b0bae88481b440db
SeongBinYoon <55236612+SeongBinYoon@users.noreply.github.com>
2020-07-18 16:53:43 +0900

    Add files via upload

--- 2562/2562/main.c
+++ 2562/2562/main.c
@@ -13,13 +13,18 @@ int main(int argc, const char * argv[]) {
     int n;
     int max = 0;
     int cnt = 0;
+    int tmp = 0;
     for(int i = 0; i < 9; i++){
         scanf("%d",&n);
         if(n > max){
+            cnt += tmp;
             max = n;
             cnt += 1;
+            tmp = 0;
         }
-        
+        else{
+            tmp += 1;
+        }
     }
     printf("%d\n", max);
     printf("%d\n", cnt);
