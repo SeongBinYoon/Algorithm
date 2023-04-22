dc819aa0223e2899bd06dc033cbc9adb0c76ace1
SeongBinYoon <yoonseongbin4925@gmail.com>
2023-04-22 17:12:57 +0900

    [ADD] 11382 Add one directory

---
+++ 11382/11382.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  11382
+//
+//  Created by SeongBinYoon on 2023/04/22.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    long a, b, c, val = 0;
+    scanf("%ld %ld %ld", &a, &b, &c);
+    val = a + b + c;
+    printf("%ld\n", val);
+}
